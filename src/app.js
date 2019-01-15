8b723cc21507684c8dbb0a50e18ff7fe136676ef
Gerry Waterston <gwaterston@hotmail.co.uk>
2019-01-15 19:13:56 +0000

    add view folder and files

--- src/app.js
+++ src/app.js
@@ -1,5 +1,8 @@
+const InstrumentFamilies = require('./models/instrument_families.js');
 
 
 document.addEventListener('DOMContentLoaded', () => {
   console.log('JavaScript Loaded');
+
+  
 });
--- src/models/instrument_families.js
+++ src/models/instrument_families.js
@@ -1,5 +1,9 @@
+const PubSub = require('../helpers/pub_sub.js');
+
 const InstrumentFamilies = function(data) {
   this.data = data;
 };
+
+
 
 module.exports = InstrumentFamilies;
